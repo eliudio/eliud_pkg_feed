79105409993fda7f450db7b3e258537ee404a52f
Eliud Berlin <info@eliud.io>
2021-05-03 14:14:16 +0100

    Bugfixes

--- lib/tools/action/post_action_handler.dart
+++ lib/tools/action/post_action_handler.dart
@@ -39,21 +39,21 @@ class PostActionHandler extends PackageActionHandler {
   void addToMe(
       BuildContext context, PostActionModel action, LoggedIn accessState) {
     executePostIt(
-        context, action, [accessState.member.documentID], accessState);
+        context, action, [accessState.member.documentID!], accessState);
   }
 
   Future<void> addToFollowers(BuildContext context, PostActionModel action,
       LoggedIn accessState) async {
-    List<String?> followers = (await followingRepository(appId: action.appID)!
+    List<String> followers = (await followingRepository(appId: action.appID)!
             .valuesListWithDetails(
                 eliudQuery: EliudQuery(theConditions: [
       EliudQueryCondition('followedId',
           isEqualTo: accessState.member.documentID)
     ])))
         .map((following) =>
-            following!.follower!.documentID)
+            following!.follower!.documentID!)
         .toList();
-    followers.add(accessState.member.documentID); // add myself
+    followers.add(accessState.member.documentID!); // add myself
     executePostIt(context, action, followers, accessState);
   }
 
@@ -63,13 +63,14 @@ class PostActionHandler extends PackageActionHandler {
   }
 
   Future<void> executePostIt(BuildContext context, PostActionModel action,
-      List<String?> readAccess, LoggedIn accessState) async {
+      List<String> readAccess, LoggedIn accessState) async {
     var modalRoute = ModalRoute.of(context) as ModalRoute;
     var settings = modalRoute.settings;
     var pageId = settings.name;
     var parameters = settings.arguments;
     // What is the current page?
     // Can we actually add the current page? (page should have an indicator if it's allowed to be added)
+
     postRepository(appId: action.appID)!.add(PostModel(
       documentID: newRandomKey(),
       author: await memberPublicInfoRepository(appId: action.appID)!.get(accessState.member.documentID),
@@ -79,7 +80,7 @@ class PostActionHandler extends PackageActionHandler {
       archived: PostArchiveStatus.Active,
       pageParameters: parameters as Map<String, dynamic>?,
       description: "Post added by Add To Post button",
-      readAccess: readAccess as List<String>?,
+      readAccess: readAccess,
     ));
   }
 }
