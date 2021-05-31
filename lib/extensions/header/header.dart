90818794686bceb8f2bbe065a4307a91da51ee5a
Eliud <58745656+eliudio@users.noreply.github.com>
2021-05-31 23:28:54 +0100

    Rich field

--- lib/extensions/header/header.dart
+++ lib/extensions/header/header.dart
@@ -1,7 +1,5 @@
-import 'package:eliud_core/model/abstract_repository_singleton.dart';
-import 'package:eliud_pkg_feed/extensions/util/avatar_helper.dart';
-import 'package:eliud_pkg_feed/extensions/util/post_helper.dart';
 import 'package:eliud_pkg_feed/extensions/util/switch_feed_helper.dart';
+import 'package:eliud_pkg_text/extensions/rich_text_dialog.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -21,8 +19,15 @@ class _HeaderState extends State<Header> {
   @override
   Widget build(BuildContext context) {
     List<Widget> rows = [];
-    rows.add(HeaderImage(switchFeedHelper: widget.switchFeedHelper,));
-    rows.add(HeaderName(switchFeedHelper: widget.switchFeedHelper,));
+    rows.add(HeaderImage(
+      switchFeedHelper: widget.switchFeedHelper,
+    ));
+    rows.add(HeaderName(
+      switchFeedHelper: widget.switchFeedHelper,
+    ));
+    rows.add(FlatButton(child: Text("Try it"), onPressed: () {
+      RichTextDialog.open(context, "Profile", (value) {}, "<b>hello</b> world");
+    }));
     return Column(children: rows);
   }
 }
