3d261450c058fa0c2fd9b769d0aa2e949a3a1c52
Eliud <58745656+eliudio@users.noreply.github.com>
2022-02-10 14:12:06 +0000

    Abstracting Wizard

--- lib/wizards/feed_wizard.dart
+++ lib/wizards/feed_wizard.dart
@@ -2,15 +2,13 @@ import 'package:eliud_core/model/app_model.dart';
 import 'package:eliud_core/model/member_model.dart';
 import 'package:eliud_core/model/menu_item_model.dart';
 import 'package:eliud_core/style/frontend/has_text.dart';
+import 'package:eliud_pkg_create/registry/new_app_wizard_info_with_action_specification.dart';
 import 'package:eliud_pkg_create/registry/registry.dart';
-import 'package:eliud_pkg_create/widgets/new_app_bloc/action_specification.dart';
 import 'package:eliud_pkg_create/widgets/new_app_bloc/builders/app_builder.dart';
 import 'package:eliud_pkg_create/widgets/new_app_bloc/builders/helpers/menu_helpers.dart';
 import 'package:eliud_pkg_create/widgets/new_app_widget.dart';
-import 'package:eliud_pkg_medium/platform/medium_platform.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
-
 import 'builders/feed/feed_page_builder.dart';
 import 'builders/feed/follow_requests_dashboard_page_builder.dart';
 import 'builders/feed/followers_dashboard_page_builder.dart';
@@ -19,7 +17,7 @@ import 'builders/feed/invite_dashboard_page_builder.dart';
 import 'builders/feed/membership_dashboard_page_builder.dart';
 import 'builders/feed/profile_page_builder.dart';
 
-class FeedWizard extends NewAppWizardInfo {
+class FeedWizard extends NewAppWizardInfoWithActionSpecification {
   static String FEED_PAGE_ID = 'feed';
   static String FEED_MENU_COMPONENT_IDENTIFIER = "feed_menu";
   static String FEED_HEADER_COMPONENT_IDENTIFIER = "feed_header";
@@ -37,44 +35,20 @@ class FeedWizard extends NewAppWizardInfo {
   static String MEMBERSHIP_COMPONENT_IDENTIFIER = "membership";
   static String PROFILE_COMPONENT_IDENTIFIER = "profile";
 
-  FeedWizard() : super('feed', 'Feed');
+  FeedWizard() : super('feed', 'Feed',  'Generate Feed');
 
   @override
-  NewAppWizardParameters newAppWizardParameters() {
-    return FeedParameters();
-  }
+  NewAppWizardParameters newAppWizardParameters() => ActionSpecificationParametersBase(requiresAccessToLocalFileSystem: false,
+    availableInLeftDrawer: true,
+    availableInRightDrawer: false,
+    availableInAppBar: false,
+    availableInHomeMenu: true,
+    available: false,
+  );
+
 
   @override
-  MenuItemModel? getMenuItemFor(AppModel app, NewAppWizardParameters parameters, MenuType type) {
-    if (parameters is FeedParameters) {
-      var feedSpecifications = parameters.feedPageSpecifications;
-      bool generate = (type == MenuType.leftDrawerMenu) && feedSpecifications.availableInLeftDrawer ||
-          (type == MenuType.rightDrawerMenu) && feedSpecifications.availableInRightDrawer ||
-          (type == MenuType.bottomNavBarMenu) && feedSpecifications.availableInHomeMenu ||
-          (type == MenuType.appBarMenu) && feedSpecifications.availableInAppBar;
-      if (generate) {
-        return menuItemFeed(app, FEED_PAGE_ID, 'Feed');
-      }
-    } else {
-      throw Exception('Unexpected class for parameters: ' + parameters.toString());
-    }
-    return null;
-  }
-
-  @override
-  Widget wizardParametersWidget(AppModel app, BuildContext context, NewAppWizardParameters parameters) {
-    if (parameters is FeedParameters) {
-      bool hasAccessToLocalFileSystem = AbstractMediumPlatform.platform!
-          .hasAccessToLocalFilesystem();
-      return ActionSpecificationWidget(
-          app: app,
-          enabled: hasAccessToLocalFileSystem,
-          actionSpecification: parameters.feedPageSpecifications,
-          label: 'Generate Feed');
-    } else {
-      return text(app, context, 'Unexpected class for parameters: ' + parameters.toString());
-    }
-  }
+  MenuItemModel getThatMenuItem(AppModel app) => menuItemFeed(app, FEED_PAGE_ID, 'Feed');
 
   List<NewAppTask>? getCreateTasks(
       AppModel app,
@@ -85,8 +59,8 @@ class FeedWizard extends NewAppWizardInfo {
       DrawerProvider leftDrawerProvider,
       DrawerProvider rightDrawerProvider,
       ) {
-    if (parameters is FeedParameters) {
-      var feedSpecifications = parameters.feedPageSpecifications;
+    if (parameters is ActionSpecificationParametersBase) {
+      var feedSpecifications = parameters.actionSpecifications;
       if (feedSpecifications.shouldCreatePageDialogOrWorkflow()) {
         List<NewAppTask> tasks = [];
         var memberId = member.documentID!;
@@ -194,20 +168,5 @@ class FeedWizard extends NewAppWizardInfo {
     if (pageType == 'profilePageId') return PROFILE_PAGE_ID;
     else if (pageType == 'pageIdProvider') return FEED_PAGE_ID;
     return null;
-  }
-}
-
-class FeedParameters extends NewAppWizardParameters {
-  late  ActionSpecification feedPageSpecifications;
-
-  FeedParameters() {
-    feedPageSpecifications = ActionSpecification(
-      requiresAccessToLocalFileSystem: false,
-      availableInLeftDrawer: true,
-      availableInRightDrawer: false,
-      availableInAppBar: false,
-      availableInHomeMenu: true,
-      available: false,
-    );
   }
 }
